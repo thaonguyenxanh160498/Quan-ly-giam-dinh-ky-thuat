546bd56115c485dd6956c77afad771ed723dd26d
thaonguyenxanh <37950039+kunhat9@users.noreply.github.com>
2020-11-12 21:14:30 +0700

    update

--- appconfig.sql
+++ appconfig.sql
@@ -11,4 +11,4 @@ INSERT INTO AppConfig(ImageLogin,ImagePanelLogin)
 VALUES ('~/Libs/Login_v3/images/bg-01.jpg','~-webkit-linear-gradient(top, #dee0f5, #0683ef)')
 GO
 INSERT INTO staff(code,username,userpassword,name,staffstatuscode)
-     VALUES ('admin','admin', N'/f9e7bsNi+c=',N'Adminstrator',1)
+     VALUES ('admin','admin', N'/f9e7bsNi+c=',N'Adminstrator',1) 
